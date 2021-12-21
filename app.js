033be3fe98a6b43002e51ac6916c9aa1e52a1d52
Namju Yun <southpoley@gmail.com>
2021-12-21 23:12:03 +0900

    ✨ toTop button and Modal Open/Close

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ var grid = document.querySelector(".sub-pics");
 var msnry = new Masonry(grid, {
   itemSelector: ".sub-pic",
   columnWidth: ".grid-sizer",
-  gutter: 10,
 });
 
 var msnry = new Masonry(".grid", {
   // options...
 });
+
+const toTop = document.querySelector(".btn-top");
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+toTop.addEventListener("click", scrollToTop);
+
+const modal = document.querySelector(".modal");
+
+function onClick(element) {
+  document.getElementById("modalImg").src = element.src;
+  modal.classList.add("modal-visible");
+  document.body.classList.add("stop-scroll");
+}
+
+const closeModal = () => {
+  modal.classList.remove("modal-visible");
+  document.body.classList.remove("stop-scroll");
+};
+
+modal.addEventListener("click", closeModal);
