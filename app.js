4fe5bbfb9e3d7f21a23c6334bc854022c49531b5
Namju Yun <southpoley@gmail.com>
2021-12-21 20:30:11 +0900

    ✨ masonry.js setup

---
+++ app.js
@@ -0,0 +1,12 @@
+// vanilla JS
+// init with element
+var grid = document.querySelector(".sub-pics");
+var msnry = new Masonry(grid, {
+  itemSelector: ".sub-pic",
+  columnWidth: ".grid-sizer",
+  gutter: 10,
+});
+
+var msnry = new Masonry(".grid", {
+  // options...
+});
